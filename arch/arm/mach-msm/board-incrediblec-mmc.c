23949203b108033d9ceb1de892a76288c921b094
Andrew Bartholomew <andrewb03@gmail.com>
2011-10-31 15:55:52 -0400

    Lower WIFI voltage

--- arch/arm/mach-msm/board-incrediblec-mmc.c
+++ arch/arm/mach-msm/board-incrediblec-mmc.c
@@ -223,7 +223,7 @@ static unsigned int incrediblec_wifi_status(struct device *dev)
 }
 
 static struct mmc_platform_data incrediblec_wifi_data = {
-	.ocr_mask		= MMC_VDD_28_29,
+	.ocr_mask    		= MMC_VDD_20_21,
 	.status			= incrediblec_wifi_status,
 	.register_status_notify	= incrediblec_wifi_status_register,
 	.embedded_sdio		= &incrediblec_wifi_emb_data,
