tiny4579/gingertiny-v2

commit c09aeae31b0b1b9f1029cee370f5e49e86abdaf4

3/5-point multitouch detection

arch/arm/mach-msm/board-incrediblec.c
@@ -641,7 +641,7 @@ struct atmel_i2c_platform_data incrediblec_atmel_ts_data[] = {
 		.config_T6 = {0, 0, 0, 0, 0, 0},
 		.config_T7 = {50, 15, 25},
 		.config_T8 = {10, 0, 20, 10, 0, 0, 5, 15},
-		.config_T9 = {139, 0, 0, 18, 12, 0, 16, 38, 3, 7, 0, 5, 2, 15, 2, 10, 25, 5, 0, 0, 0, 0, 0, 0, 0, 0, 159, 47, 149, 81, 40},
+		.config_T9 = {139, 0, 0, 18, 12, 0, 16, 38, 3, 7, 0, 5, 2, 15, 10, 10, 25, 5, 0, 0, 0, 0, 0, 0, 0, 0, 159, 47, 149, 81, 40},
 		.config_T15 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		.config_T18 = {0, 0},
 		.config_T19 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},

drivers/input/touchscreen/atmel.c
@@ -1734,6 +1734,24 @@ static int atmel_ts_probe(struct i2c_client *client,
 		ts->id->matrix_x_size, ts->id->matrix_y_size,
 		ts->id->num_declared_objects);
 
+/*
+        Begin ffolkes multitouch selection
+        Purpose: Some hardware can't support more than 2 or 3 multitouch points. Detect which hardware can support
+  what and enable the correct number of points for that device. If another touch controller is found, keep
+  the default 2 point configuration.
+*/
+        if(ts->id->family_id == 0x4F && ts->id->version == 0x16) {
+                printk(KERN_INFO "3 point multitouch enabled\n");
+                pdata->config_T9[14] = 3;
+        } else if(ts->id->family_id == 0x80 && ts->id->version == 0x16) {
+                printk(KERN_INFO "5 point multitouch enabled - NOTE: Some devices may revert to 2 point\n");
+                pdata->config_T9[14] = 5;
+  	} else
+	        printk(KERN_INFO "Standard 2 point multitouch enabled\n");
+/*
+END
+*/
+
 	/* Read object table. */
 	ret = read_object_table(ts);
 	if (ret < 0)

commit cd183f0b025ac0696d70c249a7334b2f4e0a171a

FIX issue with some panels caused by commit c09aeae

arch/arm/mach-msm/board-incrediblec.c
@@ -641,7 +641,7 @@ struct atmel_i2c_platform_data incrediblec_atmel_ts_data[] = {
 		.config_T6 = {0, 0, 0, 0, 0, 0},
 		.config_T7 = {50, 15, 25},
 		.config_T8 = {10, 0, 20, 10, 0, 0, 5, 15},
-		.config_T9 = {139, 0, 0, 18, 12, 0, 16, 38, 3, 7, 0, 5, 2, 15, 10, 10, 25, 5, 0, 0, 0, 0, 0, 0, 0, 0, 159, 47, 149, 81, 40},
+		.config_T9 = {139, 0, 0, 18, 12, 0, 16, 38, 3, 7, 0, 5, 2, 15, 2, 10, 25, 5, 0, 0, 0, 0, 0, 0, 0, 0, 159, 47, 149, 81, 40},
 		.config_T15 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 		.config_T18 = {0, 0},
 		.config_T19 = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},